8a2afbd28fac6c74038e3ebc7f3e723117762db3
Anthony Howl <anthonyhowl@protonmail.ch>
2018-07-28 17:25:09 -0700

    clean up

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -8,15 +8,7 @@ import DownloadFileList from './DownloadFileList.js';
 import BdcWrapper from './BdcWrapper.js';
 import oip from './oip.svg';
 import Lookup from './Lookup.js';
-import Multipart from './Multipart.js';
 import Artifact from './ArtifactInspec';
-import DlManager from './DownloadManager';
-import oiplogo from './oip-wordmark-and-logo.png'
-const imagestyle = {
-	textAlign: 'center',
-	marginTop: '40px'
-}
-
 
 class AppRoutes extends Component {
   render() {
@@ -29,26 +21,13 @@ class AppRoutes extends Component {
   </button>
   <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav">
-      <li className="nav-item">
-        <a className="nav-link" href="/multipart">Multipart Inspector<span className="sr-only"></span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/DlManager">Download Manager<span className="sr-only"></span></a>
-      </li>
     </ul>
   </div>
 </nav>
 					
           <Route exact path='/' render={props => <Lookup Core={this.props.Core}{...props} />} />
-          <Route path="/multipart" render={props => <Multipart Core={this.props.Core} {...props} />} />
           <Route path="/artInspec" render={props => <Artifact Core={this.props.Core} {...props} />} />
-          <Route path="/DlManager" render={props => <DlManager Core={this.props.Core} {...props} />} />
 
-
-       	<div className="image"style={imagestyle}> 
-						<img src={oiplogo} class="img-fluid" alt="Responsive image"/>
-
-					</div> 
 
     </div>
     );
